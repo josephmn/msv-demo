0c71cbbe9d0ca758c47bdb784e64166de5258007
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-11-29 22:10:34 -0500

    first commit

---
+++ src/main/java/api/demo/msv_demo/MsvDemoApplication.java
@@ -0,0 +1,13 @@
+package api.demo.msv_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvDemoApplication.class, args);
+	}
+
+}
